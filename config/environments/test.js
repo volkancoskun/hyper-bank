aa85b57d08bc88c9859dd3e71c290dbc70a1c8e6
TrasherDK <trasherdk@fumlersoft.dk>
2021-05-19 09:21:05 +0200

    fix: add path to tests - *.test.js files should only be in tests directory fix: add missing test environment

---
+++ config/environments/test.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const logPath = path.join(__dirname, '../../logs/test.log');
+
+module.exports = {
+  web: {
+    port: 3000
+  },
+  logging: {
+    appenders: { cheese: { type: 'file', filename: logPath} },
+    categories: { default: { appenders: ['cheese'], level: 'error' } }
+  }
+};
